5b47812ba08be1d89005dd5dd4dca5db6ca3294e
diwakarsharma <diwakar2034@yahoo.com>
2018-09-16 13:50:17 -0400

    Initial commit

---
+++ src/com/company/FibNumbers.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class FibNumbers {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("the first 10 numbers in the" +
+                "Fibonacci series are: ");
+        for(int i = 0; i<10 ; i++)
+            System.out.print(fib(i)+ " ");
+    }
+    public static int fib(int n){
+        if (n== 0)
+            return 0;
+        else if (n==1)
+            return 1;
+        else
+            return fib(n-1)+ fib(n-2);
+
+    }
+}
